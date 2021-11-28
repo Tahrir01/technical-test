fc0a01ca84b9c38991fac490f7eff8085143fbf9
Tahrir01 <94541881+Tahrir01@users.noreply.github.com>
2021-11-28 22:42:02 +0530

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,40 @@
+<?php
+// convert string to int
+$string = "20";
+$int_value=(Int)$string;
+var_dump($int_value);
+
+echo"<br>";
+// convert string to float
+
+$string="30";
+$float=(Float)$string;
+var_dump($float);
+
+echo"<br>";
+//convert int to string
+
+$int = 50;
+$str =(string)$int;
+var_dump($str);
+
+echo"<br>";
+// convert int to float
+
+$int = 10;
+$flo =(Float)$int;
+var_dump($flo);
+
+echo"<br>";
+//
+$flo=10.50;
+$str =(string)$flo;
+var_dump($str);
+
+echo"<br>";
+// float to integer
+$flo = 20.50;
+$int=(Int)$flo;
+var_dump($int)
+
+?>
