335a48630f8cb48b7e7e424d2144fb46d57b8890
Tahrir01 <94541881+Tahrir01@users.noreply.github.com>
2021-11-30 10:43:52 +0530

    Add files via upload

---
+++ Datatypes.php
@@ -0,0 +1,23 @@
+<?php
+#scalar(it hold only single value)
+#integer mean numeric data type
+# Eg..
+$num=100;
+echo $num."<br>";
+var_dump($num);
+
+#float data type
+# float type it hold only numeric franctional data type 
+$float = 10.5;
+echo $float."<br>";
+var_dump($float);
+
+#string data type
+#it hold only leeter and spical chracter
+$name="Tanu";
+$age="26";
+echo $name." ";
+echo $age." ";
+var_dump($name , $age);
+
+?>
